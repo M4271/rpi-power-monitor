fc0766dd6905f68a1f25da68e1d7b50c9cf67916
M4271N <martinklemm@posteo.de>
2023-04-18 07:45:45 +0200

    First working version

--- rpi_power_monitor/influx_interface.py
+++ rpi_power_monitor/influx_interface.py
@@ -17,18 +17,12 @@ client = InfluxDBClient(
 class Point:
     def __init__(self, p_type, *args, **kwargs):
         if p_type == 'home_load':
-            self.power = kwargs['power']
-            self.current = kwargs['current']
+            self.power_W = kwargs['power_W']
+            self.current_A = kwargs['current_A']
+            self.energy_kwh = kwargs['energy_kwh']
             self.p_type = p_type
             self.time = kwargs['time']
         
-        elif p_type == 'solar':
-            self.power = kwargs['power']
-            self.current = kwargs['current']
-            self.pf = kwargs['pf']
-            self.p_type = p_type
-            self.time = kwargs['time']
-            
         elif p_type == 'net': 
             '''
             This type represents the current net power situation at the time of sampling. 
@@ -37,8 +31,9 @@ class Point:
             self.p_type  : the type of point [home_load, solar, net, ct, voltage]
             self.time    : timestamp from when the data was sampled
             '''
-            self.power = kwargs['power']
-            self.current = kwargs['current']
+            self.power_W = kwargs['power_W']
+            self.current_A = kwargs['current_A']
+            self.energy_kwh = kwargs['energy_kwh']
             self.p_type = p_type
             self.time = kwargs['time']
         
@@ -51,70 +46,46 @@ class Point:
             self.ct_num  : the CT number [0-6]
             self.time    : timestamp from when the data was sampled
             '''
-            self.power = kwargs['power']
-            self.current = kwargs['current']
+            self.power_W = kwargs['power_W']
+            self.current_A = kwargs['current_A']
+            self.voltage_V = kwargs['voltage_V']
+            self.energy_kwh = kwargs['energy_kwh']
             self.p_type = p_type
             self.pf = kwargs['pf']
+            self.time = kwargs['time']
             self.ct_num = kwargs['num']
-            self.time = kwargs['time']
-
-        elif p_type == 'voltage':
-            '''
-            This type represents a voltage reading. 
-            The self.voltage is self explanatory.
-            The v_input represents the identifier of the voltage input. This is setting up for multiple voltage inputs in the future.
-            '''
-            self.voltage = kwargs['voltage']
-            self.v_input = kwargs['v_input']
-            self.time = kwargs['time']
-            self.p_type = p_type
 
     def to_dict(self):
         if self.p_type == 'home_load':
             data = {
-                "measurement": 'home_load',
+                "measurement": self.p_type,
                 "fields": {
-                    "current": self.current,
-                    "power": self.power
-                },
-                "time": self.time
-            }
-        elif self.p_type == 'solar': 
-            data = {
-                "measurement": "solar",
-                "fields": {
-                    "current": self.current,
-                    "power": self.power,
-                    "pf": self.pf
+                    "current_A": self.current_A,
+                    "power_W": self.power_W,
+                    "energy_kwh": self.energy_kwh,
                 },
                 "time": self.time
             }
         elif self.p_type == 'net':
-            if self.power < 0:
-                status = 'Producing'
-            elif self.power > 0:
-                status = 'Consuming'
-            else:
-                status = "No data"
             data = {
-                "measurement": "net",
+                "measurement": self.p_type,
                 "fields": {
-                    "current": self.current,
-                    "power": self.power,
-                },
-                "tags": {
-                    "status": status,
+                    "current_A": self.current_A,
+                    "power_W": self.power_W,
+                    "energy_kwh": self.energy_kwh,
                 },
                 "time": self.time
             }
 
         elif self.p_type == 'ct':
             data = {
-                "measurement": "raw_cts",
+                "measurement": self.p_type,
                 "fields": {
-                    "current": self.current,
-                    "power": self.power,
+                    "current_A": self.current_A,
+                    "power_W": self.power_W,
+                    "energy_kwh": self.energy_kwh,
                     "pf": self.pf,
+                    "voltage_V": self.voltage_V,
                 },
                 "tags": {
                     "ct": self.ct_num
@@ -122,17 +93,6 @@ class Point:
                 "time": self.time
             }
 
-        elif self.p_type == 'voltage':
-            data = {
-                "measurement": "voltages",
-                "fields": {
-                    "voltage": self.voltage,
-                },
-                "tags": {
-                    "v_input": self.v_input
-                },
-                "time": self.time
-            }
         else:
             return
 
@@ -156,70 +116,39 @@ def close_db():
     client.close()
 
 
-def write_to_influx(solar_power_values,
-                    home_load_values,
+def write_to_influx(home_load_values,
                     net_power_values,
                     ct1_dict,
-                    ct2_dict,
-                    ct3_dict,
-                    ct4_dict,
-                    ct5_dict,
-                    ct6_dict,
                     poll_time,
-                    length,
-                    voltages):
-    # Calculate Averages
-    avg_solar_power = sum(solar_power_values['power']) / length
-    avg_solar_current = sum(solar_power_values['current']) / length
-    avg_solar_pf = sum(solar_power_values['pf']) / length
-    avg_home_power = sum(home_load_values['power']) / length
-    avg_home_current = sum(home_load_values['current']) / length
-    avg_net_power = sum(net_power_values['power']) / length
-    avg_net_current = sum(net_power_values['current']) / length
-    ct1_avg_power = sum(ct1_dict['power']) / length
-    ct1_avg_current = sum(ct1_dict['current']) / length
-    ct1_avg_pf = sum(ct1_dict['pf']) / length
-    ct2_avg_power = sum(ct2_dict['power']) / length
-    ct2_avg_current = sum(ct2_dict['current']) / length
-    ct2_avg_pf = sum(ct2_dict['pf']) / length
-    ct3_avg_power = sum(ct3_dict['power']) / length
-    ct3_avg_current = sum(ct3_dict['current']) / length
-    ct3_avg_pf = sum(ct3_dict['pf']) / length
-    ct4_avg_power = sum(ct4_dict['power']) / length
-    ct4_avg_current = sum(ct4_dict['current']) / length
-    ct4_avg_pf = sum(ct4_dict['pf']) / length
-    ct5_avg_power = sum(ct5_dict['power']) / length
-    ct5_avg_current = sum(ct5_dict['current']) / length
-    ct5_avg_pf = sum(ct5_dict['pf']) / length
-    ct6_avg_power = sum(ct6_dict['power']) / length
-    ct6_avg_current = sum(ct6_dict['current']) / length
-    ct6_avg_pf = sum(ct6_dict['pf']) / length
-    avg_voltage = sum(voltages) / length
+                    length):
 
     # Create Points
-    home_load = Point('home_load', power=avg_home_power, current=avg_home_current, time=poll_time)
-    solar = Point('solar', power=avg_solar_power, current=avg_solar_current, pf=avg_solar_pf, time=poll_time)
-    net = Point('net', power=avg_net_power, current=avg_net_current, time=poll_time)
-    ct1 = Point('ct', power=ct1_avg_power, current=ct1_avg_current, pf=ct1_avg_pf, time=poll_time, num=1)
-    ct2 = Point('ct', power=ct2_avg_power, current=ct2_avg_current, pf=ct2_avg_pf, time=poll_time, num=2)
-    ct3 = Point('ct', power=ct3_avg_power, current=ct3_avg_current, pf=ct3_avg_pf, time=poll_time, num=3)
-    ct4 = Point('ct', power=ct4_avg_power, current=ct4_avg_current, pf=ct4_avg_pf, time=poll_time, num=4)
-    ct5 = Point('ct', power=ct5_avg_power, current=ct5_avg_current, pf=ct5_avg_pf, time=poll_time, num=5)
-    ct6 = Point('ct', power=ct6_avg_power, current=ct6_avg_current, pf=ct6_avg_pf, time=poll_time, num=6)
-    v = Point('voltage', voltage=avg_voltage, v_input=0, time=poll_time)
+    home_load = Point('home_load',
+                      power_W    = sum(home_load_values['power_W']) / length,
+                      energy_kwh = sum(home_load_values['energy_kwh']) / length,
+                      current_A  = sum(home_load_values['current_A']) / length,
+                      time=poll_time)
+    net =       Point('net',      
+                      power_W    = sum(net_power_values['power_W']) / length,
+                      energy_kwh = sum(net_power_values['energy_kwh']) / length,
+                      current_A  = sum(net_power_values['current_A']) / length,
+                      time=poll_time)
+    ct1 =       Point('ct',   
+                      power_W    = sum(ct1_dict['power_W']) / length,
+                      energy_kwh = sum(ct1_dict['energy_kwh']) / length,
+                      current_A  = sum(ct1_dict['current_A']) / length,
+                      pf         = sum(ct1_dict['pf']) / length,
+                      voltage_V  = sum(ct1_dict['voltage_V']) / length,
+                      num        = ct1_dict['num'],
+                      time=poll_time)
 
     points = [
         home_load.to_dict(),
-        solar.to_dict(),
         net.to_dict(),
         ct1.to_dict(),
-        ct2.to_dict(),
-        ct3.to_dict(),
-        ct4.to_dict(),
-        ct5.to_dict(),
-        ct6.to_dict(),
-        v.to_dict(),
     ]
+
+    #print(points)
 
     try:    
         client.write_points(points, time_precision='ms')
